e50295337738efeb8366c8a19ff2683a7b39b666
wmj <work@lmm.com>
2019-03-06 09:36:45 +0800

    add

--- db/handle.go
+++ db/handle.go
@@ -7,8 +7,8 @@ import (
 func (handle *DBHandler) GetDirectDownloadApp() string {
 	engine := handle.mysqlEngine
 	column_name := "down_load_u_i_addr"
-	sql := "select ? from domain_info where id = ?"
-	results, err := engine.Query(sql, column_name, datastruct.DefaultId)
+	sql := "select down_load_u_i_addr from domain_info where id = ?"
+	results, err := engine.Query(sql, datastruct.DefaultId)
 	if err != nil || len(results) <= 0 {
 		return ""
 	}
